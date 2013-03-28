ebdaa56a0604ce293363254cbe02f6d942af5c69
trungquan <trungquana69g@gmail.com>
2013-03-28 17:36:42 +0700

    Chuong 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
+  before_filter :not_signed_in, only: [:new, :create]
 
   def new
   	@user = User.new
@@ -64,4 +65,8 @@ class UsersController < ApplicationController
     def admin_user
       redirect_to(root_path) unless current_user.admin?
     end
-  end
+
+    def not_signed_in
+      redirect_to(root_path) unless !signed_in? 
+    end
+end
