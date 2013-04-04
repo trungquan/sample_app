1901e118fdd07844744005d6f3b2817790102fe7
trungquan <trungquana69g@gmail.com>
2013-04-04 08:47:20 +0700

    Chuong 11

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,13 +13,6 @@ describe "Static pages" do
     let(:heading) { 'Sample App'}
 
     it { should_not have_selector 'title', text: '| Home' }
-  end
-
-  describe "Help page" do
-    before { visit help_path }
-
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
 
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -35,7 +28,25 @@ describe "Static pages" do
           page.should have_selector("li##{item.id}", text: item.content)
         end
       end
+      
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 followers", href: followers_user_path(user)) }
+      end
     end
+  end
+
+  describe "Help page" do
+    before { visit help_path }
+
+    it { should have_selector('h1',    text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
